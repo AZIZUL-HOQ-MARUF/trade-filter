421ef27f981f8b5f64a8313b3a7ad31deb7d86f7
azizul <azizul.hoq@selise.ch>
2022-03-22 00:30:06 +0600

    added core module

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,26 @@
+import { NgModule, Optional, SkipSelf } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AppRoutingModule } from './app-routing.module';
+import { StructureModule } from '../structure/structure.module';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    AppRoutingModule,
+    StructureModule,
+    HttpClientModule
+  ],
+  exports: [
+    StructureModule,
+    AppRoutingModule,
+    HttpClientModule
+  ]
+})
+export class CoreModule {
+  constructor(@Optional() @SkipSelf() parent: CoreModule) {
+    if (parent) {
+      throw new Error('Core Module Has Already been Loaded, Import Core Module only in AppModule');
+    }
+  }
+}
