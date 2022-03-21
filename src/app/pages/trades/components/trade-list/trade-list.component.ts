0a1cc5d110ab3884c2b7c7dba80f07ef4dbb5be8
azizul <azizul.hoq@selise.ch>
2022-03-22 03:38:38 +0600

    data fetched for list page

--- src/app/pages/trades/components/trade-list/trade-list.component.ts
+++ src/app/pages/trades/components/trade-list/trade-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Subject } from 'rxjs';
+import { catchError, of, Subject, switchMap, take } from 'rxjs';
 import { TradeService } from '../../services/trade.service';
 
 @Component({
@@ -10,15 +10,39 @@ import { TradeService } from '../../services/trade.service';
 export class TradeListComponent implements OnInit, OnDestroy {
 
   private _unsubscribeAll: Subject<any> = new Subject();
+  private nextChangeId: string = '';
+  public stashList: any[] = [];
 
   constructor(private _tradeService: TradeService) { }
 
   ngOnInit() {
+    this._getAllData();
+  }
+
+  private _getAllData(): void {
+    this._tradeService.getAllData()
+      .pipe(
+        take(1),
+        catchError(error => {
+          return of([{ next_change_id: '' }])
+        }),
+        switchMap((response) => {
+          let data = response[0];
+          return this._tradeService.getAllData({ id: data.next_change_id });
+        })
+      ).subscribe(response => {
+        this.stashList = response[0].stashes;
+        this.nextChangeId = response[0].next_change_id;
+      })
   }
 
   ngOnDestroy(): void {
     this._unsubscribeAll.next(null);
     this._unsubscribeAll.complete();
+  }
+
+  public trackBy(item: any) {
+    return item?.id || null;
   }
 
 }
--- src/app/pages/trades/services/trade.service.ts
+++ src/app/pages/trades/services/trade.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ApiService } from 'src/app/core/services/api-service.service';
 
 @Injectable()
 export class TradeService {
 
-constructor() { }
+  constructor(private apiService: ApiService) { }
+
+  public getAllData(params?: any): Observable<any> {
+    return this.apiService.get('public-stash-tabs', params);
+  }
 
 }
